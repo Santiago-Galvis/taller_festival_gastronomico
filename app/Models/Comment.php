4b8e735c03d6f116acfac1c8483c53ded6a4c142
Santiago-Galvis <santiagogalvisgt@gmail.com>
2021-09-19 20:59:10 -0500

    Modelos Categoria, Comentario, Restaurante y Usuario creados

---
+++ app/Models/Comment.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'comment',
+        'score',
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+}
